9bc0849b0526cc58b4dc8d5eb0a807e5cebf93cb
brodrickgeorge <brodrick.george@yahoo.com>
2021-02-13 21:42:54 -0600

    card changed

--- src/components/spots/SpotSummary.js
+++ src/components/spots/SpotSummary.js
@@ -1,11 +1,15 @@
 import React from "react";
 import moment from "moment";
+import spot1 from "./img/spot1.jpg";
 
 const SpotSummary = ({ spot }) => {
   return (
     <div className="card z-depth-4 spot-summary">
-      <div className="card-content grey-text text-darken-3">
+      <div class="card-image">
+        <img src={spot1} />
         <span className="card-title">{spot.title}</span>
+      </div>
+      <div className="card-content grey-text text-darken-3">
         <p>
           Posted by {spot.authorFirstName} {spot.authorLastName}
         </p>
